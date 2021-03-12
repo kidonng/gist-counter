5a5ece7a584807624524eaa2eb215c59bc3af83f
Kid <kidonng@gmail.com>
2021-03-12 20:04:04 +0800

    Remove deps from self

--- api/github-file-contributors.ts
+++ api/github-file-contributors.ts
@@ -1,5 +1,55 @@
-import { ServerRequest, ExtendedRequest } from '../deps.ts'
-import api from 'https://cdn.jsdelivr.net/gh/kidonng/deno-scripts/scripts/github-file-contributors.ts'
+import { ServerRequest, ExtendedRequest, ky } from '../deps.ts'
+export interface getContributorsOptions {
+  user: string
+  repo: string
+  tree?: string
+  path: string
+}
+
+export interface Contributor {
+  id: string
+  username: string
+}
+
+/**
+ * Get contributors of a specific GitHub file
+ *
+ * ```ts
+ * import getContributors from 'cdn.jsdelivr.net/gh/kidonng/github-file-contributors/mod.ts'
+ *
+ * const contributors = await getContributors({
+ *   user: 'octocat',
+ *   repo: 'Hello-World',
+ *   path: '/README',
+ * })
+ * ```
+ */
+export async function getContributors({
+  user,
+  repo,
+  tree = 'master',
+  path,
+}: getContributorsOptions): Promise<Contributor[]> {
+  const html = await ky
+    .get(`https://github.com/${user}/${repo}/contributors-list/${tree}/${path}`)
+    .text()
+  const contributors = Array.from<string[]>(
+    html.matchAll(/u\/(\d+).+\s+([\w-]+)/g)
+  ).map(([, id, username]) => ({ id, username }))
+
+  return contributors
+}
+
+if (import.meta.main) {
+  const match = Deno.args[0]?.match(/github\.com\/(.+?)\/(.+?)\/(?:.+?)\/(.+?)(\/.+)/)
+  if (!match) {
+    console.error('Please enter a valid GitHub URL!')
+    Deno.exit(1)
+  }
+
+  const [, user, repo, tree, path] = match
+  console.log(await getContributors({ user, repo, tree, path }))
+}
 
 export default async (req: ServerRequest) => {
   const { search } = new ExtendedRequest(req)
@@ -12,7 +62,7 @@ export default async (req: ServerRequest) => {
     })
   }
 
-  const contributors = await api({ user, repo, tree, path })
+  const contributors = await getContributors({ user, repo, tree, path })
 
   req.respond({
     body: JSON.stringify(contributors),
--- deps.ts
+++ deps.ts
@@ -2,6 +2,7 @@
 import dayjs from 'https://jspm.dev/dayjs@1.9.6/esm'
 
 export { dayjs }
-export { ky } from 'https://cdn.jsdelivr.net/gh/kidonng/deno-utils@v0.3.0/mod.ts'
+// @deno-types="https://cdn.jsdelivr.net/npm/ky@0.27.0/index.d.ts"
+export { default as ky } from 'https://cdn.jsdelivr.net/npm/ky@0.27.0/index.js'
 export * from 'https://deno.land/std@0.79.0/http/server.ts'
 export * from 'https://deno.land/x/deno_extras@v0.4.3/mod.ts'
